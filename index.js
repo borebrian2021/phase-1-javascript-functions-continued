e58f91604c7850de43f437522aefd6fb8866b21c
Brian Kimutai <brian.kimutai@student.moringaschool.com>
2022-06-08 23:54:27 +0300

    commit tree ed2895be37dfbfe72791380da082eb4d1c3cc6f9

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+function saturdayFun(message="roller-skate"){
+
+message=`This Saturday, I want to ${message}!`
+    return message;
+
+
+}
+sartudayFun()
+
+
+sartudayFun("bathe my dog")
+
+function mondayWork(deff="go to the office"){
+var deff=`This Monday, I will ${deff}.`
+return deff;
+}
+mondayWork()
+
+function wrapAdjective(b,c,msg="You are"){
+return `${msg} ${b}${c}`
+}
+
+let result = wrapAdjective('*')
+ let emphatic = result("a hard worker")
